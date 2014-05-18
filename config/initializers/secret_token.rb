110565ae126b196ee4fc6f03f0aa123b2d026cde
Kappie <ghkapteijns@gmail.com>
2014-05-18 22:09:52 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maartje::Application.config.secret_token = '667741ef5992270eb475895bb63c37b1572e6af45ca93d01c8ca1c300aad4a8a3dd62203b8b6a1ef7d8d197eae6f43fc2fb148846213e661c084bd7813fc8256'
